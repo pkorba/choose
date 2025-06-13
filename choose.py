1b384d508dc99c69388b32329bf917b234f9e5a3
pkorba <32198274+pkorba@users.noreply.github.com>
2025-06-13 16:52:24 +0200

    Visual refresh

--- choose.py
+++ choose.py
@@ -5,12 +5,12 @@ import random
 
 class ChooseBot(Plugin):
     @command.new("choose", help="Have the bot choose for you")
-    @command.argument("choices", pass_raw=True, required=True)
-    async def choose(self, evt: MessageEvent, choices: str) -> None:
+    @command.argument("options", pass_raw=True, required=True)
+    async def choose(self, evt: MessageEvent, options: str) -> None:
         await evt.mark_read()
-        if not choices:
-            await evt.respond("Usage: !choose <choice1, choice2, choice3>")
+        if not options:
+            await evt.reply("> **Usage:** !choose <option1, option2, option3>")
             return
-        choices_array = choices.split(",")
-        choice = random.choice(choices_array).strip()
-        await evt.reply(f"I choose **{choice}**.")
+        options_array = options.split(",")
+        choice = random.choice(options_array).strip()
+        await evt.reply(f"> I choose **{choice}**.")
